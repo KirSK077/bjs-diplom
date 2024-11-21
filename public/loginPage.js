7201019e68f6e249eee5686d4792bc552e33c5d3
Kirill Kaimusov <kaimusovks@yandex.ru>
2024-11-21 19:56:46 +0500

    Итоговая работа. Исправлено: сообщения об ошибках при входе/регистрации выводится в форму интерфейса

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,22 +3,20 @@ const userForm = new UserForm();
 
 userForm.loginFormCallback = function(data) {
     ApiConnector.login(data, function(response) {
-        // console.log(response);
         if (response.success) {
             location.reload();
         } else {
-            alert(response.error);
+            userForm.setLoginErrorMessage(response.error);
         }
     });
 };
 
 userForm.registerFormCallback = function(data) {
     ApiConnector.register(data, function(response) {
-        // console.log(response);
         if (response.success) {
             location.reload();
         } else {
-            alert(response.error);
+            userForm.setRegisterErrorMessage(response.error);
         }
     });
 };
